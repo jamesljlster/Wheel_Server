5cbf67316e3e53866a6f72db42d4373518fc3f7d
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-10-05 22:10:49 +0800

    Backup Point

--- lib/wsvc.c
+++ lib/wsvc.c
@@ -1,4 +1,65 @@
-int wsvc_foo()
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "wsvc.h"
+
+char* wsvc_ip_def[] = {
+	"0.0.0.0"
+};
+
+char* wsvc_port_def[] = {
+	"7500"
+};
+
+char* wsvc_max_client_def[] = {
+	"5"
+};
+
+char* wsvc_dev_timeout_def[] = {
+	"20"
+};
+
+args_t wsvc_arg_list[] = {
+	{1, "host-ip", 'I', 1, wsvc_ip_def, "Server Setting", "Host IP dddress"},
+	{1, "host-port", 'P', 1, wsvc_port_def, "Host port"},
+	{1, "max-client", 'M', 1, wsvc_max_client_def, "Maximum client connection"},
+	{0, "dev-path", 'D', 1, NULL, "Wheel Device Setting", "Wheel device path"},
+	{0, "dev-baud", 'B', 1, NULL, "Wheel device baudrate"},
+	{1, "dev-timeout", 'T', 1, wsvc_dev_timeout_def, "Wheel device timeout"},
+	{0, "help", 'H', 0, NULL, "User Interface", "Print detail of arguments"},
+	ARGS_TERMINATE
+};
+
+int wsvc_arg_check(args_t* argList)
 {
-	return 0;
+	int ret = 0;
+
+	// Checking important arguments
+	if(argList[WSVC_DEV_PATH].enable <= 0 && argList[WSVC_DEV_PATH].leading == NULL)
+	{
+		printf("Argument \'%s\' not set!", argList[WSVC_DEV_PATH].name);
+		ret = -1;
+		goto RET;
+	}
+
+	if(argList[WSVC_DEV_BAUD].enable <= 0 && argList[WSVC_DEV_BAUD].leading == NULL)
+	{
+		printf("Argument \'%s\' not set!", argList[WSVC_DEV_BAUD].name);
+		ret = -1;
+		goto RET;
+	}
+
+RET:
+	return ret;
+}
+
+int wsvc_dev_open(wsvc_t* wsvc, args_t* argList)
+{
+	int ret = 0;
+	int tmp;
+	const char* tmpPtr;
+
+	// Parse baudrate
+
+	return ret;
 }
--- lib/wsvc.h
+++ lib/wsvc.h
@@ -1,9 +1,36 @@
 #ifndef __WSVC_H__
 #define __WSVC_H__
 
+#include <pthread.h>
+#include <Wheel.h>
+#include <args.h>
+
+enum WSVC_ARG
+{
+	WSVC_HOST_IP,
+	WSVC_HOST_PORT,
+	WSVC_MAX_CLIENT,
+	WSVC_DEV_PATH,
+	WSVC_DEV_BAUD,
+	WSVC_DEV_TIMEOUT
+};
+
+typedef struct WSVC
+{
+	WCTRL wCtrl;
+	int devTimeout;
+	pthread_mutex_t mutex;
+} wsvc_t;
+
+extern args_t wsvc_arg_list[];
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+int wsvc_arg_check(args_t* argList);
+int wsvc_dev_open(wsvc_t* wsvc, args_t* argList);
+void wsvc_dev_close(wsvc_t* wsvc);
 
 #ifdef __cplusplus
 }
