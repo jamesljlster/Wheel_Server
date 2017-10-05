53ba3dcfb91dbe3e0351a2e4a7e3643c6ab6b072
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-10-05 20:09:45 +0800

    Add deps

--- deps/deps.cmake
+++ deps/deps.cmake
@@ -1,14 +1,12 @@
 # Set dependences paths
 set(DEPS_PATHS
-	${CMAKE_CURRENT_SOURCE_DIR}/deps/deps1
-	${CMAKE_CURRENT_SOURCE_DIR}/deps/deps2
+	${CMAKE_CURRENT_SOURCE_DIR}/Wheel/src
+	${CMAKE_CURRENT_SOURCE_DIR}/tcpmgr/lib
+	${CMAKE_CURRENT_SOURCE_DIR}/args/lib
 	)
 include_directories(${DEPS_PATHS})
 
 # Find other dependences
-find_package(OpenCV REQUIRED)
-include_directories(${OpenCV_INCLUDE_DIRS})
-
 find_path(SERIALPORT_INCLUDE_DIR libserialport.h
 	"/usr/include"
 	"/usr/local/include"
